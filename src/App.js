19b58be8f8e67d4f75177dbbff1583d7910bb46b
Fabian Pena <camilo.fabis@gmail.com>
2020-09-15 16:24:15 -0400

    First iteration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <IdCard
+    img = "https://randomuser.me/api/portraits/men/20.jpg"
+    firstName = 'John'
+    lastName = 'Doe'
+    gender = 'male'
+    birth = '1992-07-14'
+    height = '178'
+   />
+   <IdCard
+    img = "https://randomuser.me/api/portraits/women/59.jpg"
+    firstName = 'Obrien'
+    lastName = 'Delores'
+    gender = 'female'
+    birth = '1988-05-11'
+    height = '172'
+   />
     </div>
   );
+}
+
+function IdCard(props) {
+  return (
+    <div>
+      <p><img src={props.img} alt="Profile Pic"/></p>
+      <p>First Name: {props.firstName}</p>
+      <p>Last Name: {props.lastName}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Birth: {props.birth}</p>
+      <p>Height: {props.height}</p>
+     
+    </div>
+  )
 }
 
 export default App;
